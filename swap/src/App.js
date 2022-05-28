8c313bd491567a56c9c1271f17b2ccaf0c36a115
Tina Chen <tinachen@tinas-mbp.ads.northwestern.edu>
2022-05-28 16:53:31 -0500

    added signup

---
+++ swap/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+//import ReactDOM from "react-dom"
+
+import './App.css';
+
+import Signup from './Signup';
+function App() {
+
+  return (
+    <div>
+      <div class="name">
+
+        <h1>swap.nu</h1>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
