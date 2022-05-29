80d1cb6ec158e275c574c1a850e8ade9754a43b1
Michael Lee <76514821+EmhuEhmu@users.noreply.github.com>
2022-05-29 01:39:34 -0500

    login feature kind of works

--- app/src/server/firebase_auth.js
+++ app/src/server/firebase_auth.js
@@ -3,14 +3,14 @@ import {initializeApp} from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_FIREBASE_DATABASEURL,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
-    appId: process.env.REACT_APP_FIREBASE_APPID,
-};
+    apiKey: "AIzaSyATewvmb2vuI5VMA4O91qIZKqfkyavqYMc",
+    authDomain: "swapnu-eca62.firebaseapp.com",
+    databaseURL: "https://swapnu-eca62-default-rtdb.firebaseio.com",
+    projectId: "swapnu-eca62",
+    storageBucket: "swapnu-eca62.appspot.com",
+    messagingSenderId: "986800582411",
+    appId: "1:986800582411:web:c692bbc6e9b335a947bc0e"
+  };
 
 const app = initializeApp(firebaseConfig);
 
