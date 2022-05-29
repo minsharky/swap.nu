da489755525607aea03bc93bdf12975b9bb29f19
tinachnn <tinachen2025@u.northwestern.edu>
2022-05-28 21:46:44 -0500

    1

--- app/src/pages/Signup.js
+++ app/src/pages/Signup.js
@@ -1,7 +1,32 @@
+import './Signup.css'
+import {Input} from 'semantic-ui-react'
+import {Button} from 'semantic-ui-react'
+import React from "react";
+
 function Signup() {
 
     return(
-        <h1> hello </h1>
+        <div>
+            <h1> JOIN SWAP.NU </h1>
+            <h2> Your details </h2>
+            <div>
+                <Input placeholder='First Name' style={{width:'200px'}}/>
+                <Input placeholder='Last Name'  style={{width:'200px'}}/>
+            </div>         
+            <div>
+                <Input placeholder='Northwestern Email' style={{width:'400px'}}/>
+            </div>
+            <h2> Create your username and details</h2>
+            <div>
+                <Input placeholder='Username' style={{width:'400px'}}/> 
+            </div>
+            <div>
+                <Input placeholder='Password' style={{width:'400px'}}/> 
+            </div>
+        <div class='btn signup'>
+            <Button name="signup">Sign Up</Button>
+        </div>
+        </div>
     );
 }
 
